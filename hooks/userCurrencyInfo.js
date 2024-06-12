6b4a336679244f5ecd7aebaa632ae36cfff51897
Mirza Usman <mirzausmanmu07842@gmail.com>
2024-06-12 21:09:36 +0500

    added background and compleeted input box

--- hooks/userCurrencyInfo.js
+++ hooks/userCurrencyInfo.js
@@ -1,12 +1,11 @@
-const { useEffect } = require("react");
-const { useState } = require("react");
-
+import { useEffect, useState } from "react";
 
 
 function useCurrencyInfo(currency){
     const [data, setData] = useState({})
     useEffect(()=>{
-        fetch(`https://cdn.hsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+        fetch(url)
        .then(res=>res.json())
        .then(res => setData(res[currency]))
        console.log(data)
